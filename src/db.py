8e85454fddae07f08e7bdc2df437192f751a65c8
joezhu1230 <103288060+joezhu1230@users.noreply.github.com>
2026-04-30 14:52:14 -0400

    Update db.py

--- src/db.py
+++ src/db.py
@@ -4,7 +4,7 @@ import sqlite3
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[1]
-DB_PATH = ROOT / "teiko.db"
+DB_PATH = ROOT / "immune_cell_data.db"
 CSV_PATH = ROOT / "cell-count.csv"
 
 
